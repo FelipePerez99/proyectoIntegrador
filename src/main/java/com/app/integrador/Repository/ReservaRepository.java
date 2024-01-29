9b7ee92cacf6e5f7dfa9414019e02d0a274251ac
Felipe <andresfppy@gmail.com>
2024-01-29 12:50:01 -0500

    Revert "Corregir errores en application.properties"
    This reverts commit 224f0c8f3fe577a1e09de568706d52c94b674260.

--- src/main/java/com/app/integrador/Repository/ReservaRepository.java
+++ src/main/java/com/app/integrador/Repository/ReservaRepository.java
@@ -1,7 +1,6 @@
 package com.app.integrador.Repository;
 
-import com.app.integrador.Entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
+public interface ReservaRepository extends JpaRepository<ReservaRepository, Integer> {
 }
