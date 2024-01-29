760dd76ae372a1c21b20c78be0def1faab10d7f1
Felipe <andresfppy@gmail.com>
2024-01-29 15:25:21 -0500

    Agregar método findByUsuarioId en ReservaRepository

--- src/main/java/com/app/integrador/Repository/ReservaRepository.java
+++ src/main/java/com/app/integrador/Repository/ReservaRepository.java
@@ -3,5 +3,8 @@ package com.app.integrador.Repository;
 import com.app.integrador.Entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
+    List<Reserva> findByUsuarioId(Integer userId);
 }
